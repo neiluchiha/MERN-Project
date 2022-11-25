ea3580edc3408ad0d2e2458efc279ba1a1f89e59
neiluchiha <nyneil93@gmail.com>
2022-11-25 18:51:22 +0530

    Footer Container

--- frontend/src/components/FooterComponent.js
+++ frontend/src/components/FooterComponent.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
 
 const FooterComponent = () =>{
-    return <p>This is Footer </p>
+    return (
+        <Container fluid>
+      <Row>
+        <Col>Copyright 2022</Col>
+      </Row>
+    </Container>
+    )
     
     }
     
